6fc3f07cb4b71ff2eeb3eb1bb75c90444791c23f
Luis Davel <luisdavel90@outlook.com>
2021-11-21 15:25:02 -0300

    v.luis

--- PRODUCAO/conexao.php
+++ PRODUCAO/conexao.php
@@ -2,5 +2,5 @@
 	$con = mysqli_connect('localhost', 'root', '', 'tcc') or die('Não foi possível conectar');
     mysqli_set_charset($con, 'utf8');
     session_start();
-    // teste
+    // teste João é gay
 ?>
